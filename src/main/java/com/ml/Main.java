f13d18d23226ece475be670ac15b74bd5fcab47e
A.Furkan Tanriverdi <ffurkan.tanriverdi@gmail.com>
2016-10-10 22:12:24 +0300

    code cleaning

--- src/main/java/com/ml/Main.java
+++ src/main/java/com/ml/Main.java
@@ -15,24 +15,8 @@ import weka.core.Instances;
 public class Main {
 	
 	public static void main(String[] args) throws Exception {
-//		
-//		 BufferedReader reader = new BufferedReader(
-//                 new FileReader("C:/Users/furkan/desktop/weather.arff"));
-//		 Instances data = new Instances(reader);
-//		 reader.close();
-//		 // setting class attribute
-//		 data.setClassIndex(data.numAttributes() - 1);
-//		 
-//		 Attribute attribudes =
-//		 
-//		
-//		 for (Instance instance : data) {
-//			
-//			 System.out.println(instance.);
-//		}
+		
 		String fileName = "C:/Users/furkan/desktop/training_subsetD.arff";
-//		Matrix matrix = new Matrix();
-//		matrix.loadArff(fileName);
 		DataTable table = new DataTable();
 		table.loadArffToDataTable(fileName);
 		System.out.println("parsed completed!");
